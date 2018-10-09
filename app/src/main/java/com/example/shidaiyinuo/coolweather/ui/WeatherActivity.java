88186609215124a1f559afd344a1561078851be4
Yieron东东 <yindongdong@shidaiyinuo.com>
2018-10-09 17:46:17 +0800

    【端】 coolWeather 【Task】学习第一行代码 【修改内容】编写天气界面 【后续跟进】

---
+++ app/src/main/java/com/example/shidaiyinuo/coolweather/ui/WeatherActivity.java
@@ -0,0 +1,15 @@
+package com.example.shidaiyinuo.coolweather.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.shidaiyinuo.coolweather.R;
+
+public class WeatherActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_weather);
+    }
+}
