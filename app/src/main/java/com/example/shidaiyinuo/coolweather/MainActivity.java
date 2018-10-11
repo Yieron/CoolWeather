2d91eab8b53d7d388b6de5edfe4ca07275f0bf0a
Yieron东东 <yindongdong@shidaiyinuo.com>
2018-10-11 09:31:58 +0800

    【端】 coolWeather 【Task】学习第一行代码 【修改内容】编写天气界面 【后续跟进】

---
+++ app/src/main/java/com/example/shidaiyinuo/coolweather/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.shidaiyinuo.coolweather;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.shidaiyinuo.coolweather.ui.WeatherActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        SharedPreferences defaultSharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+        if (defaultSharedPreferences.getString("weather",null)!=null){
+            Intent intent = new Intent(this,WeatherActivity.class);
+            startActivity(intent);
+            finish();
+        }
+    }
+}
